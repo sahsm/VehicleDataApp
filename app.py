dfae7e4fbcdbce3c3fff648dd94221420bb9ddcb
107327500+sahsm@users.noreply.github.com <107327500+sahsm@users.noreply.github.com>
2026-02-25 16:32:47 -0800

    fix column names

--- app.py
+++ app.py
@@ -18,12 +18,14 @@ hist_button = st.button("Show Mileage Distribution")
 
 if hist_button:
     st.write("Distribution of vehicle mileage (odometer readings)")
-    fig_hist = px.histogram(car_data, x="odometer")
+    fig_hist = px.histogram(car_data, x="odometer",
+                            title="Mileage Distribution")
     st.plotly_chart(fig_hist, use_container_width=True)
 
 scatter_button = st.button("Show Price vs Mileage Relationship")
 
 if scatter_button:
     st.write("Relationship between vehicle mileage and price.")
-    fig_scatter = px.scatter(car_data, x="odometer", y="price")
+    fig_scatter = px.scatter(car_data, x="odometer",
+                             y="price", title='Price vs Mileage')
     st.plotly_chart(fig_scatter, use_container_width=True)
